4afb5dfaf0e1f8df6796d4eee8d081348586b9d7
jcbowie <calebdevops@gmail.com>
2019-10-17 17:31:30 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project to ananalyze, discover and possibly predict future NFL games using hoistorical game data.',
+    author='Caleb Bowie',
+    license='',
+)
